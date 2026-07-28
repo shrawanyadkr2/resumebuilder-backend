7660444202cabc878b901aac5b8aa359f6cfa0b4
shrawan <shrawan29yadav@gmail.com>
2026-07-28 19:48:29 +0530

    Use native Spring Data Mongo auto-configuration for Atlas URI

--- src/main/java/com/shrawan/resumebuilder/config/MongoConfig.java
+++ src/main/java/com/shrawan/resumebuilder/config/MongoConfig.java
@@ -1,27 +1,10 @@
 package com.shrawan.resumebuilder.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
 @Configuration
 @EnableMongoAuditing
 public class MongoConfig {
 
-    @Value("${SPRING_DATA_MONGODB_URI:${spring.data.mongodb.uri:${MONGODB_URI:mongodb://localhost:27017/resumebuilder}}}")
-    private String mongoUri;
-
-    @Bean
-    public MongoDatabaseFactory mongoDbFactory() {
-        return new SimpleMongoClientDatabaseFactory(mongoUri);
-    }
-
-    @Bean
-    public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoDbFactory());
-    }
 }
