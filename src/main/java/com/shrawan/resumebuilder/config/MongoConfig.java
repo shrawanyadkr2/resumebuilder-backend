a57210f162d1da932b280d047b827e68643051d4
shrawan <shrawan29yadav@gmail.com>
2026-07-30 19:50:29 +0530

    fix: explicitly define MongoClient and MongoTemplate beans to guarantee MongoDB Atlas URI usage

--- src/main/java/com/shrawan/resumebuilder/config/MongoConfig.java
+++ src/main/java/com/shrawan/resumebuilder/config/MongoConfig.java
@@ -1,11 +1,28 @@
 package com.shrawan.resumebuilder.config;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 @EnableMongoAuditing
 public class MongoConfig {
 
+    @Value("${spring.data.mongodb.uri:mongodb+srv://shrawanyadkr2:Shrawan2026@cluster0.uyjza.mongodb.net/resumebuilder?retryWrites=true&w=majority}")
+    private String mongoUri;
 
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), "resumebuilder");
+    }
 }
+
